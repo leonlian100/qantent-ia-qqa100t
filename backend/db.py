66f95c2a4afadc3302da674e046c7624ac8f46d9
leonlian100 <leonliang100@gmail.com>
2026-04-25 00:43:26 +0800

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,12 @@
+import sqlite3
 
+conn = sqlite3.connect("patents.db")
+
+conn.execute("""
+CREATE TABLE IF NOT EXISTS patents (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    abstract TEXT,
+    assignee TEXT
+)
+""")
